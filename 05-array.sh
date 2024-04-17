a6d24a7b6824599f0b20ef08a6005a8ff8186a44
akhildanda <akhildanda7@gmail.com>
2024-04-17 19:56:32 +0530

    hello script

---
+++ 05-array.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+PLAYERS=("MESSI" "RONALDO" "NEYMAR" "SALAH")
+
+echo "first player is: ${players[0]}"
+echo "first player is: ${players[1]}"
+echo "first player is: ${players[2]}"
+echo "first player is: ${players[3]}"
