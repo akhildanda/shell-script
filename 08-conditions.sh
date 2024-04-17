2890822219ec0cdc06914019dc6d1cf282e00e73
akhildanda <akhildanda7@gmail.com>
2024-04-17 21:27:23 +0530

    hello script

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,9 +4,9 @@ NUMBER=$1
 
 if [ $NUMBER -gt 50 ]
 then
-    echo "Given number $NUMBER is greater than 10"
+    echo "Given number $NUMBER is greater than 50"
 else
-    echo "Given number $NUMBER is less than 10" 
+    echo "Given number $NUMBER is less than 50" 
 fi
 
 #-gt, -lt, -eq, -ge, -le
