e9b1ff41593f94222a40b105c044bd2b7a2c81f8
akhildanda <akhildanda7@gmail.com>
2024-04-20 12:12:27 +0530

    hello script

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=(echo "$0 | cut 'd'"."-f1")
+SCRIPT_NAME=(echo $0 |cut 'd' "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
@@ -23,8 +23,8 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing mysql"
 
-dnf install git -y &>>LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing git"
